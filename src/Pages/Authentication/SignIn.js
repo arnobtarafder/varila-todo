af8c05f157479b76d2f48aef129e9a4630162578
arnobtarafder <rmtarnob@gmail.com>
2022-05-19 05:17:01 +0600

    open: registration page

--- src/Pages/Authentication/SignIn.js
+++ src/Pages/Authentication/SignIn.js
@@ -1,10 +1,10 @@
 import React from 'react';
-// import Login from '../../Components/Login/Login';
+import Login from '../../Components/Login/Login';
 
 const SignIn = () => {
     return (
         <div>
-            {/* <Login /> */}
+            <Login />
         </div>
     );
 };
