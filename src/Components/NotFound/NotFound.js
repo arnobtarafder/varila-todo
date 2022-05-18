1b902e8b07bdb7f5da04a7de0a8e612cf24d02ff
arnobtarafder <rmtarnob@gmail.com>
2022-05-19 05:47:40 +0600

    add: 404 notfound page

---
+++ src/Components/NotFound/NotFound.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
+
+const NotFound = () => {
+  const navigate = useNavigate();
+
+
+  // const handleBack = () => {
+  //     window.document.re
+  // }
+
+  return (
+    <div className='NotFound-Body container-fluid h-100 w-100 py-5'>
+
+      <div id="background"></div>
+      <div className="NotFound-Top">
+        <h1 className="NotFound-Header">404</h1>
+        <h3 className='NotFound-Text'>page not found</h3>
+      </div>
+      <div className="NotFound-Container">
+        <div className="ghost-copy">
+          <div className="one"></div>
+          <div className="two"></div>
+          <div className="three"></div>
+          <div className="four"></div>
+        </div>
+        <div className="ghost">
+          <div className="face">
+            <div className="eye"></div>
+            <div className="eye-right"></div>
+            <div className="mouth"></div>
+          </div>
+        </div>
+        <div className="NotFound-Shadow"></div>
+      </div>
+      <div className="NotFound-Bottom">
+        <p className='NotFound-Paragraph'>Boo, looks like a ghost stole this page!</p>
+
+        <div className="NotFound-Buttons">
+          <button className="NotFound-Button" onClick={() => navigate(-1)}>Back</button>
+        
+          <button onClick={() => navigate("/home")} className='NotFound-Button'>home <i className="far fa-hand-point-right"></i></button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
