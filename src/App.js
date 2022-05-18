5a0cffdc39f1fbaa560b8dc3b7002c0840402474
arnobtarafder <rmtarnob@gmail.com>
2022-05-19 05:10:52 +0600

    add: navigation bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
+import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
+import Navbar from './Components/Navbar/Navbar';
+import SignUp from './Pages/Authentication/SignUp';
+import SignIn from './Pages/Authentication/SignIn';
+import Home from './Pages/Home';
 
 
 function App() {
   return (
     <div>
+        <Navbar />
+
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <PrivateRoute>
+                <Home />
+              </PrivateRoute>
+            }
+          >
+          </Route>
+
+          <Route path='/login' element={<SignIn />}></Route>
+          <Route path='/registration' element={<SignUp />}></Route>
+        </Routes>
 
 
 
-      <Toaster />
+        <Toaster />
     </div>
   );
 }
---
+++ src/Pages/Authentication/SignUp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import Registration from '../../Components/Registration/Registration';
+
+const SignUp = () => {
+    return (
+        <div>
+            {/* <Registration /> */}
+        </div>
+    );
+};
+
+export default SignUp;
